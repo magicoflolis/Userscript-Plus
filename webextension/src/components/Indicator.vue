1c27ca8db79371b6dba0bb2ee02c97a488a54074
magicoflolis <magicoflolis@gmail.com>
2021-03-24 17:35:11 -0700

    push
    Signed-off-by: magicoflolis <magicoflolis@gmail.com>

---
+++ webextension/src/components/Indicator.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Card class="circle">
+            <Badge place="count" :count="count" class-name="badge"></Badge>
+        </Card>
+    </div>
+
+</template>
+
+<script>
+ export default {
+   props: ['count']
+ }
+</script>
+
+<style>
+    @import './common/css/default.css';
+</style>
